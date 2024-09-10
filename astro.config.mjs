75fb6794c55a4008850916eb38a6cf85778b58a1
Jeff Musgrave <jeff.musgrave@gmail.com>
2024-09-10 11:17:40 +0100

    small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-// import { sharp } from 'astro/assets/services/sharp';
+import { sharp } from 'astro/assets/services/sharp';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
@@ -9,11 +9,11 @@ import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   site: 'https://designgenius.com',
-  // image: {
-  //   service: sharp,
-  //   domains: ['designgenius.com'],
-  //   remotePatterns: [{ protocol: "https" }],
-  // },
+  image: {
+    service: sharp,
+    domains: ['designgenius.com'],
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [
     tailwind(), 
     partytown({
@@ -28,28 +28,28 @@ export default defineConfig({
   ],
   redirects:{
   },
-  // vite: {
-  //   resolve: {
-  //     alias: {
-  //       '@images': '/src/img'  // Make sure this path is correct
-  //     }
-  //   }, plugins: [
-  //     {
-  //       name: 'image-optimization',
-  //       enforce: 'post',
-  //       apply: 'build',
-  //       transformIndexHtml(html) {
-  //         return html.replace(/src="([^"]+)"/g, (match, url) => {
-  //           if (url.includes('/_astro/')) {
-  //             return match;
-  //           }
-  //           const fileName = url.split('/').pop().split('.')[0];
-  //           return `src="/_astro/${fileName}.webp"`;
-  //         });
-  //       }
-  //     },
-  //   ],
-  // },
+  vite: {
+    resolve: {
+      alias: {
+        '@images': '/src/images'  // Make sure this path is correct
+      }
+    }, plugins: [
+      {
+        name: 'image-optimization',
+        enforce: 'post',
+        apply: 'build',
+        transformIndexHtml(html) {
+          return html.replace(/src="([^"]+)"/g, (match, url) => {
+            if (url.includes('/_astro/')) {
+              return match;
+            }
+            const fileName = url.split('/').pop().split('.')[0];
+            return `src="/_astro/${fileName}.webp"`;
+          });
+        }
+      },
+    ],
+  },
   content: {
     sources: ['src/content'],
     collections: {
