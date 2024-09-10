a52858efdb8e4e18a9e8c0852e43d39c88ce34db
Jeff Musgrave <jeff.musgrave@gmail.com>
2024-09-10 11:55:23 +0100

    made changes to image import

--- src/lists/portfolioItems.js
+++ src/lists/portfolioItems.js
@@ -3,92 +3,125 @@ export const portfolioItems = [
     title: "Whitewater Ski Resort",
     description:
       "When Whitewater Ski Resort asked for a new website, I created this design, which helped them look more exciting and sophisticated.",
-    thumb: "/images/portfolio/digitalprint/thumbs/WhitewaterStyle_200.jpg",
-    image: "/images/portfolio/digitalprint/WhitewaterStyle.jpg",
+    thumb: () =>
+      import("../images/portfolio/digitalprint/thumbs/WhitewaterStyle_200.jpg"),
+    image: () => import("../images/portfolio/digitalprint/WhitewaterStyle.jpg"),
   },
   {
     title: "arXiv.org",
     description:
       "How can an academic research website look serious yet stylish? Consider arXiv.org, for example. Here is how I would redesign the site to look more visually appealing, while retaining enough of the original to be recognizable.",
-    thumb: "/images/portfolio/digitalprint/thumbs/arXivConcept_200.jpg",
-    image: "/images/portfolio/digitalprint/arXivConcept.jpg",
+    thumb: () =>
+      import("../images/portfolio/digitalprint/thumbs/arXivConcept_200.jpg"),
+    image: () => import("../images/portfolio/digitalprint/arXivConcept.jpg"),
   },
   {
     title: "PyTorch Adapt",
     description:
       "Even a codebase on Github can be branded with a distinctive logo design. This logo uses a simplified and stylized chameleon with eyes to suggest artificial intelligence, while performing double duty by hinting at the library's function: domain adaptation.",
-    thumb: "/images/portfolio/digitalprint/thumbs/PyAdapt_200.jpg",
-    image: "/images/portfolio/digitalprint/PyAdapt.jpg",
+    thumb: () =>
+      import("../images/portfolio/digitalprint/thumbs/PyAdapt_200.jpg"),
+    image: () => import("../images/portfolio/digitalprint/PyAdapt.jpg"),
   },
   {
     title: "Pay Per Click Ads",
     description:
       "Here are some ad designs I created for several clients who wanted to advertise with Google AdWords.",
-    thumb: "/images/portfolio/digitalprint/thumbs/SmallGraphicsSpread_200.jpg",
-    image: "/images/portfolio/digitalprint/SmallGraphicsSpread.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/digitalprint/thumbs/SmallGraphicsSpread_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/digitalprint/SmallGraphicsSpread.jpg"),
   },
   {
     title: "University of Guelph FARE",
     description:
       "For this website, I used images of food and agriculture to visually communicate the focus of research for a professor at the University of Guelph.",
-    thumb: "/images/portfolio/digitalprint/thumbs/UniversitySpread_200.jpg",
-    image: "/images/portfolio/digitalprint/UniversitySpread.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/digitalprint/thumbs/UniversitySpread_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/digitalprint/UniversitySpread.jpg"),
   },
   {
     title: "KCM Whitepaper",
     description:
       "How can a designer make a whitepaper look appealing? Here is an attractive design I created for a marketing whitepaper from KCM.",
-    thumb: "/images/portfolio/digitalprint/thumbs/WhitePaperSpread_200.jpg",
-    image: "/images/portfolio/digitalprint/WhitePaperSpread.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/digitalprint/thumbs/WhitePaperSpread_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/digitalprint/WhitePaperSpread.jpg"),
   },
 ];
 
 export const albumArtItems = [
   {
     title: "Ephemeral Firelight",
-    thumb: "/images/portfolio/albumart/thumbs/EphemeralFirelight_200.jpg",
-    image: "/images/portfolio/albumart/EphemeralFirelight.jpg",
+    thumb: () =>
+      import("../images/portfolio/albumart/thumbs/EphemeralFirelight_200.jpg"),
+    image: () => import("../images/portfolio/albumart/EphemeralFirelight.jpg"),
   },
   {
     title: "Half The Truth Is A Whole Lie",
-    thumb:
-      "/images/portfolio/albumart/thumbs/HalfTheTruthIsAWholeLieArt_200.jpg",
-    image: "/images/portfolio/albumart/HalfTheTruthIsAWholeLieArt.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/albumart/thumbs/HalfTheTruthIsAWholeLieArt_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/albumart/HalfTheTruthIsAWholeLieArt.jpg"),
   },
   {
     title: "LP One",
-    thumb: "/images/portfolio/albumart/thumbs/LPOne_200.jpg",
-    image: "/images/portfolio/albumart/LPOne.jpg",
+    thumb: () => import("../images/portfolio/albumart/thumbs/LPOne_200.jpg"),
+    image: () => import("../images/portfolio/albumart/LPOne.jpg"),
   },
   {
     title: "Melody In The Half Light",
-    thumb: "/images/portfolio/albumart/thumbs/MelodyInTheHalfLight_200.jpg",
-    image: "/images/portfolio/albumart/MelodyInTheHalfLight.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/albumart/thumbs/MelodyInTheHalfLight_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/albumart/MelodyInTheHalfLight.jpg"),
   },
   {
     title: "Protection",
-    thumb: "/images/portfolio/albumart/thumbs/SV1_03_Protection_200.jpg",
-    image: "/images/portfolio/albumart/SV1_03_Protection.jpg",
+    thumb: () =>
+      import("../images/portfolio/albumart/thumbs/SV1_03_Protection_200.jpg"),
+    image: () => import("../images/portfolio/albumart/SV1_03_Protection.jpg"),
   },
   {
     title: "Tilt",
-    thumb: "/images/portfolio/albumart/thumbs/SV1_05_Tilt_200.jpg",
-    image: "/images/portfolio/albumart/SV1_05_Tilt.jpg",
+    thumb: () =>
+      import("../images/portfolio/albumart/thumbs/SV1_05_Tilt_200.jpg"),
+    image: () => import("../images/portfolio/albumart/SV1_05_Tilt.jpg"),
   },
   {
     title: "I Can't Give Everything Away",
-    thumb:
-      "/images/portfolio/albumart/thumbs/SV1_06_ICantGiveEverythingAway_200.jpg",
-    image: "/images/portfolio/albumart/SV1_06_ICantGiveEverythingAway.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/albumart/thumbs/SV1_06_ICantGiveEverythingAway_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/albumart/SV1_06_ICantGiveEverythingAway.jpg"),
   },
   {
     title: "Yellow Brick Road",
-    thumb: "/images/portfolio/albumart/thumbs/YellowBrickRoad_200.jpg",
-    image: "/images/portfolio/albumart/YellowBrickRoad.jpg",
+    thumb: () =>
+      import("../images/portfolio/albumart/thumbs/YellowBrickRoad_200.jpg"),
+    image: () => import("../images/portfolio/albumart/YellowBrickRoad.jpg"),
   },
   {
     title: "You'd Be So Nice To Come Home To",
-    thumb: "/images/portfolio/albumart/thumbs/YoudBeSoNiceToComeHomeTo_200.jpg",
-    image: "/images/portfolio/albumart/YoudBeSoNiceToComeHomeTo.jpg",
+    thumb: () =>
+      import(
+        "../images/portfolio/albumart/thumbs/YoudBeSoNiceToComeHomeTo_200.jpg"
+      ),
+    image: () =>
+      import("../images/portfolio/albumart/YoudBeSoNiceToComeHomeTo.jpg"),
   },
 ];
